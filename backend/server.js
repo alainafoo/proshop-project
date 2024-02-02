f616aa7d50f7bb3ebd0f91287d8115e45aa6a13c
Alaina <Alaina@DESKTOP-L3S3EJF>
2024-02-02 14:57:27 -0500

    added user routes + controller, email/pass validation, jwt http cookie, middleware, encryption

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,12 +14,19 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send(('API is running...'))
 });
 
 app.use('/api/products', productRoutes);
-
+app.use('/api/users', userRoutes);
 app.use(notFound); // Middleware to handle 404 errors
 app.use(errorHandler); // Middleware to handle all other errors
 
