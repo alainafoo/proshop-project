c80cce81641e7aab521640afd24fb7d4af02bc18
Alaina <Alaina@DESKTOP-L3S3EJF>
2024-02-15 11:22:48 -0500

    added order routes and controller

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+import { 
+    addOrderItems, 
+    getMyOrders, 
+    getOrderById, 
+    updateOrderToPaid, 
+    updateToDelivered, 
+    getOrders  } from '../controllers/orderController.js';
+
+router.route('/').post(protect,addOrderItems).get(protect, admin, getOrders);
+router.route('/mine').get(protect,getMyOrders);
+router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/:id/deliver').put(protect, admin, updateToDelivered);
+
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 app.use(notFound); // Middleware to handle 404 errors
 app.use(errorHandler); // Middleware to handle all other errors
 
