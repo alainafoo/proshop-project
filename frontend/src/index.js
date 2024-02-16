fc6fc206db488e77db0dc62d9e2cd80d1643916d
Alaina <Alaina@DESKTOP-L3S3EJF>
2024-02-16 18:16:27 -0500

    added admin functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store';
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingScreen from './screens/ShippingScreen';
-import OrderListScreen from './screens/admin/OrderListScreen';
-
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import {
-createBrowserRouter,
-createRoutesFromElements, 
-Route, 
-RouterProvider
-
-} from 'react-router-dom'
-
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+// import { HelmetProvider } from 'react-helmet-async';
+import PrivateRoute from './components/PrivateRoute';
+import AdminRoute from './components/AdminRoute';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import AdminRoute from './components/AdminRoute';
-
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import PrivateRoute from './components/PrivateRoute';
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import OrderListScreen from './screens/admin/OrderListScreen';
+import ProductListScreen from './screens/admin/ProductListScreen';
+import ProductEditScreen from './screens/admin/ProductEditScreen';
+import UserListScreen from './screens/admin/UserListScreen';
+import UserEditScreen from './screens/admin/UserEditScreen';
+import store from './store';
+import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const router = createBrowserRouter(
 
@@ -52,6 +53,11 @@ const router = createBrowserRouter(
 
       <Route path ="" element={<AdminRoute/>}>
         <Route path ="/admin/orderlist" element={<OrderListScreen/>}/>
+        <Route path ="/admin/productlist" element={<ProductListScreen/>}/>
+        <Route path ="/admin/product/:id/edit" element={<ProductEditScreen/>}/>
+        
+        <Route path ="/admin/user/:id/edit" element={<UserEditScreen/>}/>
+        <Route path ="/admin/userlist" element={<UserListScreen/>}/>
       </Route>
     </Route>
 
