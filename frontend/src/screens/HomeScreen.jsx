bfee0dd87036b09a3d26ea58d1354949d5a7e76b
Alaina <Alaina@DESKTOP-L3S3EJF>
2024-02-28 23:52:52 -0500

    implemented paginate components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,8 +3,12 @@ import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/loader';
 import Message from '../components/message';
+import { useParams } from 'react-router-dom';
+import Paginate from '../components/Paginate';
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({pageNumber});
 
   return (
     <>
@@ -14,12 +18,13 @@ const HomeScreen = () => {
     ) : error ? (<Message variant = 'danger'>{error?.data?.message || error.error}</Message>) : (<>
       <h1>Latest Products</h1>
       <Row>
-        {products.map((product) => (
+        {data.products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product = {product}/>
           </Col>
         ))}
       </Row>
+      <Paginate pages={data.pages} page={data.page} isAdmin={false}/>
     </>) }
   
     </>
