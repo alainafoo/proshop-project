73c075e69e5b1dd39642c98c91ee2251a3821919
alainafoo <74021209+alainafoo@users.noreply.github.com>
2024-03-01 17:27:13 -0500

    Update errorMiddleware.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -8,11 +8,6 @@ const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode; // If the status code is 200, set it to 500, otherwise use the status code that was passed in
     let message = err.message;
 
-    if(err.name === 'CastError' && err.kind === 'ObjectId') {
-        statusCode = 404;
-        message = 'Resource not found';
-    }
-
     res.status(statusCode).json({
         message, 
         stack: process.env.NODE_ENV === 'production' ? null : err.stack, // If we're in production, don't show the stack trace
